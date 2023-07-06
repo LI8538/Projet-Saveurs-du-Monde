8dc1392f52f4a364055c7d3b75faa3526ffe4901
LI8538 <61384579+LI8538@users.noreply.github.com>
2023-07-06 08:29:01 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', function(){
+    const header =document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0 );
+});
+
+function toggleMenu(){
+    const tmenuToggle = document.querySelector('.menuToggle');
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+}
+
+
+
+
+// réservation
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+// close modal function
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// open modal function
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
